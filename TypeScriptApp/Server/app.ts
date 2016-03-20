5487bc18308b20a7814a017dca680d0a155aa851
David Squire <squiredd@gmail.com>
2016-03-20 16:47:13 -0400

    trying to remove generated .js files

--- TypeScriptApp/Server/app.ts
+++ TypeScriptApp/Server/app.ts
@@ -1,19 +1,19 @@
 ﻿/// <reference path="typings/main.d.ts"/>
 
-//var http = require('http');
-//http.createServer(function (req, res) {
-//    res.writeHead(200, {
-//        'Content-Type': 'text/html'
-//    });
-//    res.write('<!doctype html>\n<html lang="en">\n' +
-//        '\n<meta charset="utf-8">\n<title>Test web page on node.js</title>\n' +
-//        '<style type="text/css">* {font-family:arial, sans-serif;}</style>\n' +
-//        '\n\n<h1>Euro 2012 teams</h1>\n' +
-//        '<div id="content"><p>The teams in Group D for Euro 2012 are:</p><ul><li>England</li><li>France</li><li>Sweden</li><li>Ukraine</li></ul></div>' +
-//        '\n\n');
-//    res.end();
-//}).listen(8888, '127.0.0.1');
-//console.log('Server running at http://127.0.0.1:8888');
+import http = require('http');
+http.createServer(function (request:http.IncomingMessage, response:any) {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    response.write('<!doctype html>\n<html lang="en">\n' +
+        '\n<meta charset="utf-8">\n<title>Test web page on node.js</title>\n' +
+        '<style type="text/css">* {font-family:arial, sans-serif;}</style>\n' +
+        '\n\n<h1>Euro 2012 teams</h1>\n' +
+        '<div id="content"><p>The teams in Group D for Euro 2012 are:</p><ul><li>England</li><li>France</li><li>Sweden</li><li>Ukraine</li></ul></div>' +
+        '\n\n');
+    response.end();
+}).listen(8888, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:8888');
 
 import express = require('express');
 var app = express();
@@ -28,3 +28,4 @@ var server = app.listen(3000, function () {
 
     console.log('Example app listening at http://%s:%s', host, port);
 });
+
--- environment/steps.bat
+++ environment/steps.bat
@@ -1,4 +1,7 @@
-cd homeapp
+rem install git and clone repo
+rem install node
+
+cd %~dp0%\.. rem change to batch file directory
 npm install typescript -g
 npm install typings -g
 
